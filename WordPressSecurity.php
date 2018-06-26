6888149edd741b3ed64cc7dcb41afb78986cf975
Peter Pfeufer <ppfeufer@gmail.com>
2018-06-26 16:25:29 +0200

    Version Number fixed

--- WordPressSecurity.php
+++ WordPressSecurity.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/ppfeufer/pp-wp-basic-security
  * GitHub Plugin URI: https://github.com/ppfeufer/pp-wp-basic-security
  * Description: Removing all non needed stuff from the HTML Output
- * Version: 0.1-r20171102
+ * Version: 0.1-r20180626
  * Author: H.-Peter Pfeufer
  * Author URI: https://ppfeufer.de
  * License: GPLv3
