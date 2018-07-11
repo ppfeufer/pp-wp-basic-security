3f960c39dcf473077db93ac7b4f2f9c8f0e3d408
Peter Pfeufer <ppfeufer@gmail.com>
2018-07-11 10:30:13 +0200

    Replacing the default login error message with something not so revealing

---
+++ Libs/Login.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright (C) 2017 H.-Peter Pfeufer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace WordPress\Plugin\PP_WP_Basic_Security\Libs;
+
+\defined('ABSPATH') or die();
+
+class Login implements \WordPress\Plugin\PP_WP_Basic_Security\Libs\Interfaces\GenericInterface {
+
+    public function __construct() {
+        $this->execute();
+    }
+
+    public function execute() {
+        \add_filter('login_errors', [$this, 'removeLoginErrorMessages']);
+    }
+
+    public function removeLoginErrorMessages() {
+        return \__('Ups! Something went wrong!', 'pp-wp-basic-security');
+    }
+}
